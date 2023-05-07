bb9a39e7359118949d155693b0cb1172dc5761c2
John-Dag Addy <jaedagy@gmail.com>
2023-05-07 04:14:24 +0000

    fix: switch to vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { ChakraProvider } from "@chakra-ui/react"
 import theme from "./utils/theme"
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
-import Ghana from "./pages/countries/Ghana"
 import "./App.css"
 import UK from "./pages/countries/UK"
-import USA from "./pages/countries/USA"
 import Navigation from "./pages/Navigation"
-import Other from "./pages/countries/Other"
 import BackButton from "./pages/BackButton"
 
 export const App = () => (
@@ -17,10 +14,7 @@ export const App = () => (
       <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/ghana" element={<Ghana />} />
-        <Route path="/uk" element={<UK />} />
-        <Route path="/usa" element={<USA />} />
-        <Route path="/other" element={<Other />} />
+    
       </Routes>
     </BrowserRouter>
   </ChakraProvider>
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+import dns from 'dns'
+
+// https://vitejs.dev/config/
+
+dns.setDefaultResultOrder('verbatim')
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      open: true,
+      port: 3000,
+    },
+    build: {
+      sourcemap: true, // Source map generation must be turned on
+    },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+    ],
+  }
+})
